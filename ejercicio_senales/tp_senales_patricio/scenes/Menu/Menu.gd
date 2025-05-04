5c08374e4c69a06efb1d4c6c11f21da34c2b3864
Patricio <patricio_cristaldo@outlook.com>
2025-05-04 17:01:16 -0300

    Conectar manualmente (con .connect) la escena “Menu” con las instancias

--- ejercicio_senales/tp_senales_patricio/scenes/Menu/Menu.gd
+++ ejercicio_senales/tp_senales_patricio/scenes/Menu/Menu.gd
@@ -5,8 +5,13 @@ const INSTANCES_TEXT = "Cantidad de instancias: "
 var instancesCount = 0
 
 func _ready():
+	var red_generator = get_node("RedGenerator")
+	var violet_generator = get_node("VioletGenerator")
+	red_generator.connect("button_down", self.count_new_instance)
+	violet_generator.connect("button_down", self.count_new_instance)
 	# Inicializa el texto de "CountLabel" usando el texto
 	# Constante "INSTANCES_TEXT" más el valor de "instancesCount" como Texto
+	add_to_group("blocks")
 	$CountLabel.text = INSTANCES_TEXT + str(instancesCount)
 	pass
 func count_new_instance():
